a02bf97496f498df156f6b069afe94402278d252
Durdica Stancl <durdicastancl7@gmail.com>
2025-07-14 14:24:55 +0200

    Podjela projekta na osnovne pakete: controller, domain (Hardware, Type), dto, service, repository, configuration

---
+++ src/main/java/com/example/HardwareStepByStep/domain/Hardware.java
@@ -0,0 +1,48 @@
+package com.example.HardwareStepByStep.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Hardware {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "typeId")
+    private Type type;
+    private String code;
+    private long stock;
+    private BigDecimal price;
+
+   /* public Hardware(HardwareDTO hardwareDTO) {
+        this.name = hardwareDTO.getName();
+        this.type = hardwareDTO.getType();
+        this.code = hardwareDTO.getCode();
+        this.stock = hardwareDTO.getStock();
+        this.price = hardwareDTO.getPrice();
+    }*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hardware hardware = (Hardware) o;
+        return stock == hardware.stock && Objects.equals(name, hardware.name) && type == hardware.type && Objects.equals(code, hardware.code) && Objects.equals(price, hardware.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type, code, stock, price);
+    }
+}
